f5ba49a2a3360a8d976ce2054adf45c4c18f09a0
rchaurasiya1075 <rchaurasiyaself@gmail.com>
2025-08-19 17:42:38 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# Game Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+BOARD_SIZE = 600
+FPS = 60
+
+# Colors
+RED = (255, 50, 50)
+GREEN = (50, 255, 50)
+BLUE = (50, 50, 255)
+YELLOW = (255, 255, 50)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_GREY = (220, 220, 220)
+
+# Paths (relative to the project root)
+ASSET_IMAGE_BOARD = "assets/images/board.png"
+ASSET_IMAGE_RED_TOKEN = "assets/images/red_token.png"
